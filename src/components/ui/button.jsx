07a9aba89ad87c6f927078d5f326a75751cd4a80
Gatsby01k <vladyslavddos@gmail.com>
2025-08-28 17:39:02 +0300

    Update button.jsx

--- src/components/ui/button.jsx
+++ src/components/ui/button.jsx
@@ -1,10 +1,13 @@
-import React from 'react'
+import React from "react"
 
-export function Button({ className = '', asChild = false, children, ...props }) {
+export function Button({ className = "", asChild = false, children, ...props }) {
   const Comp = asChild ? 'a' : 'button'
   return (
     <Comp
-      className={`inline-flex items-center justify-center px-4 py-2 text-sm font-medium rounded-md transition disabled:opacity-50 disabled:pointer-events-none ${className}`}
+      className={
+        "inline-flex items-center justify-center px-4 py-2 text-sm font-medium rounded-2xl transition " +
+        "disabled:opacity-50 disabled:pointer-events-none select-none " + className
+      }
       {...props}
     >
       {children}
